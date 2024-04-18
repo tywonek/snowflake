6e6f5f0c895889842c7b13e0be3fafa5a174de3c
tywonek <47417026+tywonek@users.noreply.github.com>
2024-04-18 20:11:06 +0200

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,16 +1,16 @@
--- Create database if not exists GIT_TEST;
--- Create database if not exists GIT_TEST2;
--- Create database if not exists GIT_TEST3;
--- Create database if not exists GIT_TEST4;
--- Create database if not exists GIT_TEST5;
--- Create database if not exists GIT_TEST6;
--- Create database if not exists GIT_TEST7;
+Create database if not exists GIT_TEST;
+Create database if not exists GIT_TEST2;
+Create database if not exists GIT_TEST3;
+Create database if not exists GIT_TEST4;
+Create database if not exists GIT_TEST5;
+Create database if not exists GIT_TEST6;
+Create database if not exists GIT_TEST7;
 
 
-DROP DATABASE GIT_TEST;
-DROP DATABASE GIT_TEST2;
-DROP DATABASE GIT_TEST3;
-DROP DATABASE GIT_TEST4;
-DROP DATABASE GIT_TEST5;
-DROP DATABASE GIT_TEST6;
-DROP DATABASE GIT_TEST7;
+-- DROP DATABASE GIT_TEST;
+-- DROP DATABASE GIT_TEST2;
+-- DROP DATABASE GIT_TEST3;
+-- DROP DATABASE GIT_TEST4;
+-- DROP DATABASE GIT_TEST5;
+-- DROP DATABASE GIT_TEST6;
+-- DROP DATABASE GIT_TEST7;
