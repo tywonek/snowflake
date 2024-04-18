3378327ff02f3cc56dfce59e6a1527a4fd434892
tywonek <47417026+tywonek@users.noreply.github.com>
2024-04-18 20:23:10 +0200

    new files

--- create_databases.sql
+++ create_databases.sql
@@ -5,12 +5,3 @@ Create database if not exists GIT_TEST4;
 Create database if not exists GIT_TEST5;
 Create database if not exists GIT_TEST6;
 Create database if not exists GIT_TEST7;
-
-
--- DROP DATABASE GIT_TEST;
--- DROP DATABASE GIT_TEST2;
--- DROP DATABASE GIT_TEST3;
--- DROP DATABASE GIT_TEST4;
--- DROP DATABASE GIT_TEST5;
--- DROP DATABASE GIT_TEST6;
--- DROP DATABASE GIT_TEST7;
