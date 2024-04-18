c94c363970b06b9d626f838e92fa939a548d2a55
tywonek <47417026+tywonek@users.noreply.github.com>
2024-04-18 19:02:17 +0200

    Update test.sql

--- test.sql
+++ test.sql
@@ -2,3 +2,4 @@ Create database if not exists GIT_TEST;
 Create database if not exists GIT_TEST2;
 Create database if not exists GIT_TEST3;
 Create database if not exists GIT_TEST4;
+Create database if not exists GIT_TEST5;
